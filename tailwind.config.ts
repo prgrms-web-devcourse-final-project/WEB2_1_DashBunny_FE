503a2e651224a7ec4e87d565fb32ec8dab6200ef
yonghwna <yongdev0273@gmail.com>
2024-11-17 21:36:14 +0900

    ✨ Feat: 개발환경 준비
    - 라이브러리 설치(zustand, react-query)
    - tailwind 디자인토큰 생성(tailwind.config.ts)
    - eslint, prettier 적용.
     Close #2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,51 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        // 블랙
+        black: {
+          900: '#111927',
+          700: '#384250',
+          500: '#667085',
+          400: '#9DA4AE',
+        },
+        // 브라운
+        brown: {
+          200: '#B6B3B3',
+        },
+        // 오렌지
+        orange: {
+          700: '#FF7122',
+        },
+        // 그린
+        green: {
+          700: '#458D65',
+          200: '#E6FEF0',
+        },
+      },
+      fontSize: {
+        // Logo
+        logo: '60px', // 로고
+
+        // Heading
+        h1: '25px', // 큰 제목
+        h2: '20px', // 중간 제목
+        h3: '18px', // 작은 제목
+
+        // Body
+        lg: '17px', // 큰 본문
+        md: '16px', // 기본 본문
+        sm: '15px', // 작은 본문
+        xs: '13px', // 매우 작은 본문
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
