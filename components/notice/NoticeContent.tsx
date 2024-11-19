eb75fb58f1a371b22f3642588ac181bb7b7db851
HSCHEOL <knr0013@gmail.com>
2024-11-19 16:48:56 +0900

    ✨ Feat:공지 관리 페이지 퍼블리싱

--- components/notice/NoticeContent.tsx
+++ components/notice/NoticeContent.tsx
@@ -1,11 +1,14 @@
 const NoticeContent = () => {
   return (
     <section>
-      <div className="flex items-center">
+      <div className="flex items-center mb-5">
         <p className="font-semibold text-2xl ">전체 공지</p>
         <p className="font-semibold text-2xl mx-3 text-BunnyOrange"> {11}</p>
       </div>
-      <div></div>
+      <div className="bg-slate-100 w-full h-8 border-b-2 border-gray-500"></div>
+      <main className="w-full h-[68vh] border-b-4">
+        {/* 데이터가 들어올 곳 */}
+      </main>
     </section>
   );
 };
