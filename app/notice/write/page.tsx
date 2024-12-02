543de3db9a631647bae74a803eaa588d992be62f
HSCHEOL <knr0013@gmail.com>
2024-12-02 14:39:13 +0900

    ✨ Feat:react-quill-new 에디터 연동

--- app/notice/write/page.tsx
+++ app/notice/write/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-import "react-quill/dist/quill.snow.css";
+import "react-quill-new/dist/quill.snow.css";
 
-const QuillNoSSRWrapper = dynamic(() => import(`react-quill`), {
+const QuillNoSSRWrapper = dynamic(() => import(`react-quill-new`), {
   ssr: false,
   loading: () => <p>Loading</p>,
 });
@@ -11,7 +11,27 @@ const QuillNoSSRWrapper = dynamic(() => import(`react-quill`), {
 const NoticeWrite = () => {
   const [value, setValue] = useState("");
 
-  return <div></div>;
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
+
+  return (
+    <div className="flex flex-col items-center justify-center p-5">
+      <input
+        className="w-full  text-2xl p-5 border-2 rounded-xl mb-10"
+        placeholder="Title"
+      />
+
+      <div className=" w-full">
+        <QuillNoSSRWrapper
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          style={{ height: "60vh" }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default NoticeWrite;
